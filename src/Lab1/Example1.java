dbf88da7914bd37d44e2db30e7759896ea89eefc
Linkazet <linkazet25@gmail.com>
2022-11-27 14:00:06 +0500

    Лабораторная работа №1

--- src/Lab1/Example1.java
+++ src/Lab1/Example1.java
@@ -2,6 +2,6 @@ package Lab1;
 
 public class Example1 {
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        System.out.println("Привет мир! ");
     }
 }
